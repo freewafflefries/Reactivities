83637e676c51cc5ecfb5d38c5ac3926b311646ad
Scott W <scottaza@gmail.com>
2021-08-06 17:48:15 -0700

    Security headers added

--- API/Startup.cs
+++ API/Startup.cs
@@ -60,11 +60,33 @@ namespace API
         {
             app.UseMiddleware<ExceptionMiddleware>(); //made our own Exception handling Middleware.
 
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opt => opt.NoReferrer());
+            app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+            app.UseXfo(opt => opt.Deny());
+            app.UseCsp(opt => 
+                opt.BlockAllMixedContent()
+                    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+                    .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+                    .FormActions(s => s.Self())
+                    .FrameAncestors(s => s.Self())
+                    .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                    .ScriptSources(s => s.Self().CustomSources("sha256-Rh8MTEBtFMyFx6O4lsoOVvHhuLlcTgXK7ygQ7YPzl5A="))
+                );
+
             if (env.IsDevelopment())
             {
                 //app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+            else
+            {
+                app.Use(async (context, next) => 
+                {
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                    await next.Invoke();
+                });
             }
 
             //app.UseHttpsRedirection();
